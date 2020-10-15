c9fc0cccbc28b0699fbf737e45cb236a65f7d80f
Akitha Nethmal <akithanethmal@gmail.com>
2020-10-15 23:00:36 +0530

    create

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  text = "default text";
+  onChangeText() {
+    this.text = "change!";
+  }
 
   constructor() {}
-
 }
